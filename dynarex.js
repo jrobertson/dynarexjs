a3623118d1e05d7d882141c319f095a9ffcef7c1
Your Name <james@r0bertson.co.uk>
2011-09-20 12:18:04 +0100

    implemented the data island renderer.

--- dynarex.js
+++ dynarex.js
@@ -1,44 +1,101 @@
 // file: dynarex.js
 
-function loadXMLDoc(url) {
+// requires jaxle.js, and jfr.js
 
-  if (window.XMLHttpRequest)
-    xhttp=new XMLHttpRequest();
-  else
-    xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+function Dynarex(file) {
 
-  xhttp.open("GET",url,false);
-  xhttp.send("");
-  return xhttp.responseXML;
-}
-
-function fetchDynarex(file) {
-
-  xml=loadXMLDoc(file);
+  xml = new Jaxle(file).root;
 
   var dynarex = new Object();
 
   dynarex.summary = [];
   summary = xml.firstChild.childNodes[0];
 
-  for (j = 0; j < summary.childNodes.length; j++){
-    x = summary.childNodes[j];
-    dynarex.summary[x.nodeName] = x.firstChild.nodeValue;
-  }
+  // fetch the dynarex summary
+  summary.elements().each(function(item){
+    dynarex.summary[item.nodeName] = item.text;
+  });
   
-  dynarex.records = [];
+  dynarex.records = new rb.Array();
   records = xml.firstChild.childNodes[1];
 
-  for (k = 0; k < records.childNodes.length; k++){
-    rec = records.childNodes[k];
+  // fetch the dynarex records
+  records.elements().each(function(rec){
+
     var record = [];
 
-    for (var i = 0; i < rec.childNodes.length; i++) {
-      var field = rec.childNodes[i];
-      record[field.nodeName] = field.firstChild.nodeValue;
-    }
-    dynarex.records[k] = record;  
-  }
+    rec.elements().each(function(field){
+      record[field.nodeName] = field.text();
+    });
+    //dynarex.records[k] = record;
+    dynarex.records.push(record);    
+  });
 
+  // return the dynarex object
   return dynarex;
 }
+
+
+// -- start of dynarex data islands --------
+
+function findHTMLRecordToClone(element) {
+
+  parent = element.parentNode;
+  parentName = new rb.String(parent.nodeName).downcase();
+
+  switch (parentName) {
+    case 'body' : 
+      return null; 
+      break;
+    case 'tr' : 
+      return parent; 
+      break;
+    default :
+      return findHTMLRecordToClone(parent);
+  }
+}
+
+function dataIslandRender(file, node) {
+
+  dynarex = Dynarex(file);
+  recOriginal = findHTMLRecordToClone(node);
+  
+  if (recOriginal) {
+
+    // get a reference to each element containing the datafld attribute
+    var destNodes = [];
+    
+    dynarex.records.each(function(record){
+    
+      rec = recOriginal.cloneNode(true);
+
+      rec.xpath('//span[@datafld]').each(function(span){
+        destNodes[span.attribute('datafld').downcase()] = span;
+      });
+
+      for (field in destNodes){
+        if (record[field] == null) continue;
+        destNodes[field].innerHTML = record[field];
+      }    
+
+      recOriginal.parentNode.appendChild(rec);
+    });
+
+    recOriginal.parentNode.removeChild(recOriginal);
+  }
+
+}
+
+function dataIslandInit(){
+  document.xpath("//object[@type='text/xml']").each( function(x){
+    xpath = "//*[@datasrc='#" + x.attribute('id').to_s() + "']"
+    document.xpath(xpath).each(function(island){
+      node = island.element('//span[@datafld]');
+      dataIslandRender(x.attribute('data').to_s(), node);
+    });
+  });
+}
+
+dynarexDataIsland = {init: dataIslandInit}
+//dynarexDataIsland.init();
+//-- end of dynarex data islands
